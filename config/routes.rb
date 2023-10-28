bf086ae4c473fa446a3ca963448fd917e2aaf99f
EducaTec01 <l20211851@tectijuana.edu.mx>
2023-10-27 21:03:34 -0700

    configuracio de rutas login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :welcome, only: [:index]
-  get 'welcome/index'
+  root 'welcome#index'
+  
+  
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
