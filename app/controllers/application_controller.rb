8f85e6c57c2fd86e00d0c8feed439b1fa8e80ca8
EducaTec01 <l20211851@tectijuana.edu.mx>
2023-11-06 14:44:54 -0800

    Cerrar sesion implementada

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_action :authenticate_user!
+
+    def after_sign_out_path_for(resource)
+        # Personaliza la ruta a la que quieres redirigir después de cerrar sesión
+        root_path
+    end
 end
