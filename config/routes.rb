4ee99be47e9973cd9fed98599bc8046e36f5db02
xEdgarosx <edgarwhy10@gmail.com>
2023-11-20 21:56:12 -0800

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
+  get 'navbar/home'
+  get 'navbar/about'
+  get 'navbar/contact'
   resources :formularios
   resources :areas
   resources :departamentos
   resources :welcome, only: [:index]
   root 'welcome#index'
-  
-  
+
+
   devise_for :users
 
-  devise_scope :user do  
-    get '/users/sign_out' => 'devise/sessions#destroy'     
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
